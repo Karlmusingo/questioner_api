0900b51d4f810d8cce7054db2b7b565b504b3135
karl musingo <karlemusingo@gmail.com>
2019-01-02 15:57:09 +0200

    correcting the port

--- app.js
+++ app.js
@@ -265,7 +265,7 @@ app.get('/api/v1/questions/:id', (req, res) => {
 });
 
 const PORT = 5000;
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`server running on port ${PORT}`)
 });
 module.exports = app;
